2f47a4f0a1cf5516b5b501ccf4e6e083eae03a7a
Nils Nahooy <nils.nahooy@live.com>
2020-10-03 17:42:20 +0200

    added Character.show_stats()

--- Assets/Characters.py
+++ Assets/Characters.py
@@ -1,3 +1,6 @@
+from texttable import Texttable
+
+
 class Character:
     def __init__(self, first_name, race, age, sex, last_name=None, job=None):
         self.first_name = first_name.title()
@@ -12,7 +15,21 @@ class Character:
         self.sex = sex.upper()
         self.level = 1
         self.stats = {"strength": 0, "wits":0, "charisma": 0, "agility": 0, "luck": 0, "dexterity": 0}
-        
+        self.max_health = 1
+        self.max_stamina = 1
+        self.health = 1
+        self.stamina = 1
+
+    def show_stats(self):
+        stats_table = Texttable()
+        stats_table.set_deco(Texttable.HEADER)
+        stats_table.set_cols_dtype(["t", "t", "i", "i", "i", "i", "i", "i",]) 
+        stats_table.set_cols_align(["r", "r", "r", "r", "r", "r", "r", "r"])
+        stats_table.add_rows([["Health", "Stamina", "Strength", "Wits", "Charisma", "Agility", "Luck", "Dexterity"],
+                        [f"{self.health}/{self.max_health}", f"{self.stamina}/{self.max_stamina}",
+                        self.stats["strength"], self.stats["wits"], self.stats["charisma"],
+                        self.stats["agility"], self.stats["luck"], self.stats["dexterity"]]])
+        print(stats_table.draw())
 
 
 class Player(Character):
@@ -27,6 +44,22 @@ class Player(Character):
             self.stats["agility"] = 2
             self.stats["luck"] = 2
             self.stats["dexterity"] = 3
+        elif self.race == "dwarf":
+            self.max_health = 120
+            self.max_stamina = 75
+            self.stats["strength"] = 5
+            self.stats["wits"] = 2
+            self.stats["charisma"] = 1
+            self.stats["agility"] = 1
+            self.stats["luck"] = 2
+            self.stats["dexterity"] = 4
+
+        self.health = self.max_health
+        self.stamina = self.max_stamina
 
     def __str__(self):
         return f"Player {self.full_name}, a level {self.level} {self.race}."
+
+    def add_stat_point(self, stat, points=1):
+        self.stats[stat] += points
+        print(f"Your {stat} is now {self.stats[stat]}.")
--- app.py
+++ app.py
@@ -2,5 +2,7 @@ from GameTools.Dice import GameDice
 from Assets.Characters import Player
 
 d20 = GameDice(20)
-you = Player('Nils', 'Human', 33, 'M', last_name='Nahooy')
+you = Player("Nils", "Human", 33, "M", last_name="Nahooy")
 print(you)
+you.add_stat_point("strength", points=5)
+you.show_stats()
