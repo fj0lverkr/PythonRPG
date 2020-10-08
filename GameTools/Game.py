54f879cc7fc0985b208deb8fbda751a169966194
Nils Nahooy <nils.nahooy@live.com>
2020-10-08 22:10:31 +0200

    added output cosole colors and some optimizations to the code

--- GameTools/Game.py
+++ GameTools/Game.py
@@ -7,7 +7,24 @@ def clear_scene():
         _ = system('cls') 
     # for other os
     else: 
-        _ = system('clear') 
+        _ = system('clear')
+
+
+class ConsoleColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+HELP_STRINGS = ["help", "?", "what do i do?", "help me", "actions", "show actions", "hint", "show hint"]
+HELP_PROMPT = f"{ConsoleColors.OKGREEN}\nThese are the available actions:{ConsoleColors.ENDC}\n"
+ACTION_FAILED = f"{ConsoleColors.FAIL}\nThat didn't work...{ConsoleColors.ENDC}\n"
+EXIT_GAME_MESSAGE = "\nExiting game by player request..."
 
 class GameCell:
     def __init__(self, game, id, intro, prompt, actions=None):
@@ -46,27 +63,27 @@ class GameCell:
         print(self.intro_text)
         while True:
             player_action = input(self.prompt)
-            if player_action.lower() in ["help", "?", "what do i do?", "help me", "actions", "show actions", "hint", "show hint"]:
+            if player_action.lower() in HELP_STRINGS:
                 clear_scene()
-                print("\nThese are the available actions:\n")
+                print(HELP_PROMPT)
                 actions = ""
                 for i, a in enumerate(self.actions):
                     if i < len(self.actions)-1:
                         actions += f"{a['name']}, "
                     else:
                         actions += a["name"]
-                print(actions)
+                print(ConsoleColors.OKGREEN + actions + ConsoleColors.ENDC)
                 self.run_cell()
             else:
                 for a in self.actions:
                     if a['name'].lower() == player_action.lower():
                         self.run_action(a["type"], a["target"])
-                print("\nThat didn't work...\n")
+                print(ACTION_FAILED)
 
     def run_action(self, actiontype, target):
         if actiontype == "game_exit" and target == "self":
             clear_scene()
-            print("\nExiting game by player request...")
+            print(EXIT_GAME_MESSAGE)
             exit()
         elif actiontype == "goto_cell" and target in self.game.cells:
             clear_scene()
@@ -74,8 +91,9 @@ class GameCell:
 
 
 class Game:
-    def __init__(self, data_file):
+    def __init__(self, data_file, player=None):
         self.data = md.parse(data_file)
+        self.player = player
         self.cells = {}
         self.params = {}
         params = self.data.getElementsByTagName("param")
@@ -98,8 +116,3 @@ class Game:
         # if we have no savefile present (to be implemented), otherwise use the entry from the savefile
         if not saved_entry:
             self.goto_cell(self.params["entry"])
-
-
-if __name__ == "__main__":
-    sample_game = Game("GameData/sample_game.xml")
-    sample_game.start_game()
