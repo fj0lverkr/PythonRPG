e706d579dbba1f55a06e7770de452a605f337bdb
Nils Nahooy <nils.nahooy@live.com>
2020-10-08 22:09:49 +0200

    Added the code to run the game to app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from GameTools.Dice import GameDice
 from Assets.Characters import Player
+from GameTools.Game import Game
 
 d20 = GameDice(20)
 you = Player("Nils", "Human", 33, "M", last_name="Nahooy")
 print(you)
 you.add_stat_point("strength", points=5)
 you.show_stats()
+
+if __name__ == "__main__":
+    sample_game = Game("GameData/sample_game.xml", player=you)
+    sample_game.start_game()
+    
